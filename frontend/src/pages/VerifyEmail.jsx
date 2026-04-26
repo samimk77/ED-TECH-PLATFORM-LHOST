a71c731ad20ef88acf309ea6077fa37de49638f6
samimk77 <samimkausar240@gmail.com>
2026-04-27 01:30:18 +0530

    fix VerifyEmail import

--- frontend/src/pages/VerifyEmail.jsx
+++ frontend/src/pages/VerifyEmail.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { data, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import { apiConnector } from "../services/apiConnector";
 import { categories } from "../services/apis";
